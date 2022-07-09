a908a8204a772cd466d99d06f9d580ff7320d1bb
jyefddz <1065697146@qq.com>
2022-07-09 20:08:10 +0800

    切换页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Class from '@/01/Class.vue'
+import Index from '@/01/Index.vue'
+import My from '@/01/My.vue'
+import Orders from '@/01/Orders.vue'
+import NotFound from '@/01/NotFound.vue'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/index' },
+  { path: '/index',name:'index', component: Index },
+  { path: '/class', component: Class },
+  { path: '/orders', component: Orders },
+  { path: '/my', component: My },
+  { path: '*', component: NotFound }
+]
+
+let routerPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(err => err)
+}
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/附加练习_1.切换页面.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <router-link to="/index">首页</router-link>
+      <router-link to="/class">分类</router-link>
+      <router-link to="/orders">订单</router-link>
+      <router-link to="/my">我的</router-link>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.router-link-active {
+  color: red;
+}
+</style>
